bada12b2db34c0c060639cb02e6866c38ffaea77
evanwherchek <evanhsignin@gmail.com>
2024-05-31 11:46:01 -0500

    Created first screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,53 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+interface Style {
+  backdrop: React.CSSProperties
+  textfield: React.CSSProperties
+  button: React.CSSProperties
+}
+
+const styles: Style = {
+  backdrop: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    background: 'linear-gradient(90deg, rgba(22,45,103,1) 5%, rgba(40,84,167,1) 95%)'
+  },
+  textfield: {
+    background: '#FFFFFF',
+    borderRadius: '10px',
+    width: '35%',
+    margin: '20px'
+  },
+  button: {
+    borderRadius: '25px',
+    margin: '20px',
+    textTransform: 'none'
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.backdrop}>
+      <TextField
+          style={styles.textfield}
+          label="Job posting URL"
+          id="filled-size-normal"
+          variant="filled"
+          InputProps={{
+            disableUnderline: true
+          }}
+        />
+        <Button
+          style={styles.button} 
+          variant="contained">
+            Find skills
+        </Button>
     </div>
   );
 }
