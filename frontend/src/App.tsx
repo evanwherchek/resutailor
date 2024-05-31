ab5af703d7a268580c7d15aecfbdf5b04b97e202
evanwherchek <evanhsignin@gmail.com>
2024-05-31 15:01:35 -0500

    Fixed linting errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,9 @@ import Button from '@mui/material/Button';
 import React from 'react';
 
 interface Style {
-  backdrop: React.CSSProperties
-  textfield: React.CSSProperties
-  button: React.CSSProperties
+  backdrop: React.CSSProperties;
+  textfield: React.CSSProperties;
+  button: React.CSSProperties;
 }
 
 const styles: Style = {
@@ -15,38 +15,36 @@ const styles: Style = {
     justifyContent: 'center',
     alignItems: 'center',
     height: '100vh',
-    background: 'linear-gradient(90deg, rgba(22,45,103,1) 5%, rgba(40,84,167,1) 95%)'
+    background: 'linear-gradient(90deg, rgba(22,45,103,1) 5%, rgba(40,84,167,1) 95%)',
   },
   textfield: {
-    background: "#FFFFFF",
-    borderRadius: "10px",
+    background: '#FFFFFF',
+    borderRadius: '10px',
     width: '35%',
-    margin: '20px'
+    margin: '20px',
   },
   button: {
     borderRadius: '25px',
     margin: '20px',
-    textTransform: 'none'
-  }
+    textTransform: 'none',
+  },
 };
 
 function App() {
   return (
     <div style={styles.backdrop}>
       <TextField
-          style={styles.textfield}
-          label="Job posting URL"
-          id="filled-size-normal"
-          variant="filled"
-          InputProps={{
-            disableUnderline: true
-          }}
-        />
-        <Button
-          style={styles.button} 
-          variant="contained">
-            Find skills
-        </Button>
+        style={styles.textfield}
+        label="Job posting URL"
+        id="filled-size-normal"
+        variant="filled"
+        InputProps={{
+          disableUnderline: true,
+        }}
+      />
+      <Button style={styles.button} variant="contained">
+        Find skills
+      </Button>
     </div>
   );
 }
