81880f7bfeb9ddef2ca9d4563155b2d7e47777d1
GitHub Action <action@github.com>
2024-06-29 15:30:55 +0000

    Fix lint errors

--- frontend/src/tests/EnterManually.test.tsx
+++ frontend/src/tests/EnterManually.test.tsx
@@ -7,28 +7,56 @@ describe('EnterManually', () => {
   const setDescriptionMock = jest.fn();
 
   test('renders correctly', () => {
-    render(<EnterManually findSkillsClick={findSkillsClickMock} backClick={backClickMock} description='' setDescription={setDescriptionMock} />);
+    render(
+      <EnterManually
+        findSkillsClick={findSkillsClickMock}
+        backClick={backClickMock}
+        description=""
+        setDescription={setDescriptionMock}
+      />,
+    );
     expect(screen.getByLabelText('Job description')).toBeInTheDocument();
     expect(screen.getByText('Find skills')).toBeInTheDocument();
     expect(screen.getByText('Back')).toBeInTheDocument();
   });
 
   test('calls findSkillsClick when input is valid', () => {
-    render(<EnterManually findSkillsClick={findSkillsClickMock} backClick={backClickMock} description='' setDescription={setDescriptionMock} />);
+    render(
+      <EnterManually
+        findSkillsClick={findSkillsClickMock}
+        backClick={backClickMock}
+        description=""
+        setDescription={setDescriptionMock}
+      />,
+    );
     fireEvent.change(screen.getByLabelText('Job description'), { target: { value: 'Example description' } });
     fireEvent.click(screen.getByText('Find skills'));
     expect(findSkillsClickMock).toHaveBeenCalled();
   });
 
   test('does not call findSkillsClick when input is invalid', () => {
-    render(<EnterManually findSkillsClick={findSkillsClickMock} backClick={backClickMock} description='' setDescription={setDescriptionMock} />);
+    render(
+      <EnterManually
+        findSkillsClick={findSkillsClickMock}
+        backClick={backClickMock}
+        description=""
+        setDescription={setDescriptionMock}
+      />,
+    );
     fireEvent.change(screen.getByLabelText('Job description'), { target: { value: '   ' } });
     fireEvent.click(screen.getByText('Find skills'));
     expect(findSkillsClickMock).not.toHaveBeenCalled();
   });
 
   test('calls backClick when "Back" is clicked', () => {
-    render(<EnterManually findSkillsClick={findSkillsClickMock} backClick={backClickMock} description='' setDescription={setDescriptionMock} />);
+    render(
+      <EnterManually
+        findSkillsClick={findSkillsClickMock}
+        backClick={backClickMock}
+        description=""
+        setDescription={setDescriptionMock}
+      />,
+    );
     fireEvent.click(screen.getByText('Back'));
     expect(backClickMock).toHaveBeenCalled();
   });
